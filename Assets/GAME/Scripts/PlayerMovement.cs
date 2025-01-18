ae71a4b2a23bfb5a784958401b4c7b8a24645b9a
Mevlüt Sever <mevlutsever98@gmail.com>
2025-01-18 18:03:26 +0300

    new movement

--- Assets/GAME/Scripts/PlayerMovement.cs
+++ Assets/GAME/Scripts/PlayerMovement.cs
@@ -3,20 +3,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Move")]
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float rotationSpeed = 720f;
-    [SerializeField] private float jumpForce = 5f;
-    private bool isGrounded;
+    public float moveSpeed = 5f; // Normal hareket hï¿½zï¿½
+    public float dashSpeed = 20f; // Dash sï¿½rasï¿½nda uygulanacak hï¿½z
+    public float dashDuration = 0.2f; // Dash sï¿½resi
+    public float dashCooldown = 1f; // Dash iï¿½in bekleme sï¿½resi
 
-    [Header("Dash")]
-    public float dashDistance = 5f;
-    public float dashCooldown = 1f;
-    private bool canDash = true;
+    private float dashTime; // Dash sï¿½resi takibi
+    private bool isDashing = false;
+    private float dashCooldownTimer; // Dash cooldown sï¿½resi takibi
 
-   
     private Rigidbody rb;
-    private Vector3 lastMoveDirection;
+    private Vector3 moveDirection;
 
     void Start()
     {
@@ -24,91 +21,46 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update()
-    {       
-        Jump();
+    {
+        // Hareket yï¿½nï¿½nï¿½ al
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
 
+        // Dash iï¿½lemi baï¿½lat
+        if (Input.GetKeyDown(KeyCode.Space) && dashCooldownTimer <= 0)
+        {
+            isDashing = true;
+            dashTime = dashDuration;
+            dashCooldownTimer = dashCooldown;
+        }
+
+        // Cooldown timer'ï¿½ gï¿½ncelle
+        if (dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
     }
 
     void FixedUpdate()
     {
-        MovePlayer();
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (isDashing)
         {
-            StartCoroutine(Dash());
+            // Dash sï¿½rasï¿½nda hï¿½z artï¿½rï¿½lï¿½r
+            rb.linearVelocity = moveDirection * dashSpeed;
+            dashTime -= Time.fixedDeltaTime;
+
+            if (dashTime <= 0)
+            {
+                isDashing = false;
+                rb.linearVelocity = Vector3.zero; // Dash sonrasï¿½ hï¿½z sï¿½fï¿½rlanï¿½r
+            }
+        }
+        else
+        {
+            // Normal hareket
+            rb.linearVelocity = moveDirection * moveSpeed;
         }
     }
-
-
-    void MovePlayer()
-    {
-        // Klavyeden giriþ al
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // Hareket vektörünü hesapla
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
-
-        // Hareket et
-        rb.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-        // Oyuncunun yönünü hareket yönüne doðru döndür
-        if (movement != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
-        }
-    }
-
-    void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;  // Havadayken tekrar zýplamayý engelle
-        }
-    }
-
-    IEnumerator Dash()
-    {
-        canDash = false;
-
-        // Dash yönünü belirle
-        Vector3 dashDirection = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            dashDirection = Vector3.forward;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            dashDirection = Vector3.back;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            dashDirection = Vector3.left;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            dashDirection = Vector3.right;
-        }
-
-        dashDirection = transform.TransformDirection(dashDirection.normalized);
-
-        // Dash hareketini yap
-        Vector3 dashPosition = transform.position + dashDirection * dashDistance;
-        rb.MovePosition(dashPosition);
-
-        yield return new WaitForSeconds(dashCooldown);
-        canDash = true;
-    }
-
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
 }
+
